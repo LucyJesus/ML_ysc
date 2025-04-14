1de3f3c3312ed3d76c6a3547492e37a996af94e7
LucyJesus <ysc109251096@outlook.com>
2025-04-15 00:24:16 +0800

    简单更改

---
+++ DecisionTree/Credits.py
@@ -0,0 +1,32 @@
+import src
+
+"""
+age: 0:青年:1:中年:2:老年
+work:0:没有工作:1:有工作
+house:0:有房子:1:没有房子
+credit:0:信誉情况一般:1:信誉情况良好:2:信誉情况非常好
+类别为是否同意贷款
+"""
+def createDataSet():
+    dataSet = [[0, 0, 0, 0, 'no'],                        #数据集
+               [0, 0, 0, 1, 'no'],
+                [0, 1, 0, 1, 'yes'],
+                [0, 1, 1, 0, 'yes'],
+                [0, 0, 0, 0, 'no'],
+                [1, 0, 0, 0, 'no'],
+                [1, 0, 0, 1, 'no'],
+                [1, 1, 1, 1, 'yes'],
+                [1, 0, 1, 2, 'yes'],
+                [1, 0, 1, 2, 'yes'],
+                [2, 0, 1, 2, 'yes'],
+                [2, 0, 1, 1, 'yes'],
+                [2, 1, 0, 1, 'yes'],
+                [2, 1, 0, 2, 'yes'],
+                [2, 0, 0, 0, 'no']]
+    labels = ['age', 'work', 'house', 'credit']        #属性标签
+    return dataSet, labels                             #返回数据集和分类属性
+
+dataSet, labels = createDataSet()
+featLabels = []
+myTree = src.createTree(dataSet, labels, featLabels)
+print(myTree)
--- DecisionTree/PreLens.py
+++ DecisionTree/PreLens.py
@@ -6,11 +6,13 @@ a[0].split('\n')[:-1][0].split('\t')
 
 def datapro(filepath):
     data = []
-    with open(filepath,'r') as f:
-        files = f.readlines()
-        for line in files:
-            line = line.split('\n')[:-1][0].split('\t')
-            data.append(line)
+    with open(filepath, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            fields = line.replace('\t', ' ').split()
+            data.append(fields)
     return data
 
 labels = ['age of the patient','spectacle prescription','astigmatic','tear production rate']
